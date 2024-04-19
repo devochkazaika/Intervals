63fdbf314a8dc1a6a85795756b3b6eced1b39de5
Panarin Ilya <cccthafcdh@gmail.com>
2024-04-19 23:04:23 +0700

    delete swagger

--- src/main/java/com/intervals/service/IRepository.java
+++ src/main/java/com/intervals/service/IRepository.java
@@ -3,7 +3,6 @@ package com.intervals.service;
 import com.intervals.entities.Interval;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IRepository {
     <T> List<T> get(String kind);
